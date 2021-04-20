0a45151dc56f70ab18a4e7a879b7315104064076
Bryan He <bryanhe@stanford.edu>
2021-04-19 17:43:54 -0700

    Bugfix for loading videos (flipped height/width)

--- echonet/utils/__init__.py
+++ echonet/utils/__init__.py
@@ -36,7 +36,7 @@ def loadvideo(filename: str) -> np.ndarray:
     frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    v = np.zeros((frame_count, frame_width, frame_height, 3), np.uint8)
+    v = np.zeros((frame_count, frame_height, frame_width, 3), np.uint8)
 
     for count in range(frame_count):
         ret, frame = capture.read()
@@ -44,7 +44,7 @@ def loadvideo(filename: str) -> np.ndarray:
             raise ValueError("Failed to load frame #{} of {}.".format(count, filename))
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        v[count] = frame
+        v[count, :, :] = frame
 
     v = v.transpose((3, 0, 1, 2))
 
